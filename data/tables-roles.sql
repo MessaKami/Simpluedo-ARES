997d0ba21c0c87c07cd2c39f4db26ec08642d5a7
Aurore Reynier <reynier.aurore@gmail.com>
2024-11-21 16:44:54 +0100

    feat(tables-roles): resume table roles commands
    resume tables roles command with populate tables

--- data/tables-roles.sql
+++ data/tables-roles.sql
@@ -75,3 +75,81 @@ GRANT USAGE, SELECT ON SEQUENCE roles_role_id_seq, characters_character_id_seq, 
 
 
 -- A présent on se déconnecte du super admin pour switcher sur l'admin
+
+-- Puis rajouter les données dans les tables
+INSERT INTO roles (role_name) VALUES
+ ('maitre_du_jeu'),
+ ('detective'),
+ ('observateur');
+
+
+ INSERT INTO characters (character_name, character_color, character_image, character_description) VALUES
+ ('Mademoiselle Rose', '#a52a2a','https://static.wikia.nocookie.net/cluedo/images/e/e1/05-21---Miss-Scarlett.jpg/revis
+ ion/latest?cb=20171015125800&path-prefix=fr','Mademoiselle Rose âgée de 25 ans, d''origine Française et Italienne est
+  une actrice dans l''âme, de grande notoriété elle est venue rejoindre sa mère, Madame Pervenche au Manoir, avec qui 
+ elle ne s''entend pas pour fuir les tabloïds.'),
+ ('Colonel Moutarde','#ffbf4f','https://static.wikia.nocookie.net/cluedo/images/f/f7/05-22---Colonel-Mustard.jpg/revis
+ ion/latest?cb=20171015125500&path-prefix=fr','Colonel Moutarde, veille connaissance de Charles Édouard, a ses habitud
+ es au Manoir, dans lequel il passe de nombreux week-ends. Le Docteur Lenoir, neveu de Charles Édouard compte parmi se
+ s amis. Après avoir tenté de faire carrière dans l''armée, il s''est persuadé d''être un héros militaire.'),
+ ('Docteur Orchidée','#ce5e6f','https://static.wikia.nocookie.net/cluedo/images/e/eb/05-23---Miss-Orchid.jpg/revision/
+ latest?cb=20171017115616&path-prefix=fr','Le Docteur Orchidée est la fille adoptive du Dr Lenoir. Adoptée à l''adoles
+ cence, Dr Orchidée fit ses études en Suisse, avant d''être expulsée par son établissement à cause d''un empoissonneme
+ nt accidentel de sa part... Elle est surdouée.'),
+ ('Révérand Olive', '#8ba888','https://static.wikia.nocookie.net/cluedo/images/d/db/05-24---Rev-Green.jpg/revision/lat
+ est?cb=20171017113209&path-prefix=fr','Olive est décrit comme quelqu''un qui vit dans la société mondaine réussissant
+  à obtenir des invitations pour n''importe quelle soirée V.I.P. Cela traduit qu''il a des contacts et qu''il est mali
+ n. Toutefois, on sait qu''il baigne également dans d''étranges affaires si on lui demande un service.'),
+ ('Madame Pervenche','#415e9a','https://static.wikia.nocookie.net/cluedo/images/7/75/C8QfsioWsAAv_L5.jpg/revision/late
+ st?cb=20171016164322&path-prefix=fr','Madame Pervenche, de son vrai nom Éléonore Fleur, est une politicienne réputée.
+  Madame Pervenche est une femme déterminée. Elle aime briller en société et est engagée dans toutes sortes de causes 
+ qui lui permettent d''être sous le feu des projecteurs.'),
+ ('Professeur Violet','#7214c8','https://static.wikia.nocookie.net/cluedo/images/0/06/05-26---Professor-Plum.jpg/revis
+ ion/latest?cb=20171015130240&path-prefix=fr','Après avoir terminé ses études à l''école de philosophique d''Alexandri
+ e, il est parti en voyage dans des pays exotiques avec sa femme, Mlle Annette. Cependant, celui ci se délaissa rapide
+ ment de la compagnie de sa bien-aimée au profit d''une toute nouvelle venue : la jeune et charmante Mlle Rose.');
+
+INSERT INTO objects (
+     object_name,
+     object_image,
+     object_description
+     ) VALUES 
+     ('Poignard', 'https://static.wikia.nocookie.net/cluedo/images/8/8a/2016W2.jpg/revision/latest?cb=20200927104245',
+  'C''est un couteau.'),
+     ('Revolver', 'https://static.wikia.nocookie.net/cluedo/images/c/cb/2016W1.jpg/revision/latest?cb=20200927104244',
+  'C''est un flingue.'),
+     ('Chandelier', 'https://static.wikia.nocookie.net/cluedo/images/d/d9/2016W5.jpg/revision/latest?cb=20200927104246
+ ', 'C''est un chandelier.'),
+     ('Corde', 'https://static.wikia.nocookie.net/cluedo/images/c/c5/2016W4.jpg/revision/latest?cb=20200927104246', 'C
+ ''est une corde.'),
+     ('Clé anglaise', 'https://static.wikia.nocookie.net/cluedo/images/7/72/2016W6.jpg/revision/latest?cb=202009271042
+ 47', 'C''est une clé anglaise.'),
+     ('Tuyau de fer', 'https://static.wikia.nocookie.net/cluedo/images/1/17/2016W3.jpg/revision/latest?cb=202009271042
+ 45', 'C''est un tuyau de fer.');
+
+INSERT INTO rooms (room_name, room_image, room_description) VALUES
+ ('La cuisine','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est une cuisine
+ '),
+ ('La salle de bal','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est une sa
+ lle de bal'),
+ ('La véranda','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est une véranda
+ '),
+ ('La salle de billard','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est un
+ e salle de billard'),
+ ('La bibliothèque','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est une bi
+ bliothèque'),
+ ('Le bureau','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est un bureau'),
+ 
+ ('Le hall','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est un hall'),
+ ('Le salon','https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est un salon'),
+ ('La salle à manger', 'https://fr.shopping.rakuten.com/photo/936907516.jpg?frz-width=532&frz-fit=contain','C''est une
+  salle à manger');
+
+INSERT INTO users (user_pseudo,character_id,role_id) VALUES
+ ('BetaPickle',5,2),
+ ('MiloSasuke',1,2),
+ ('RoxxorStone',3,2),
+ ('PhantomCaptain',4,2),
+ ('VirusHulk',6,2),
+ ('ZeCyber',2,2),
+ ('ShadowCrazy',NULL,3);
